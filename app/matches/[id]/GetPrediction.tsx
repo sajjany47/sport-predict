7fba6f8402487335d6e7ab4a3e905c3289097495
QbAnurag <anurag.patra@qbent.in>
2025-06-27 18:47:09 +0530

    feat: add GetPrediction component with initial structure for match prediction data

---
+++ app/matches/[id]/GetPrediction.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const GetPrediction = () => {
+  // stadium Last 10 matches --------
+  // Pitch Type---------------
+  // Avg Score---------------
+  // Wheather Type----------
+  // Temprature-------------
+  // Both Team Recent Match-----
+  // H2H stats------------
+  // H2H Recent Match -------
+  // Batting First Win-----
+  // Bowling First Win----
+  // Last 20 Match Fantasy Point----
+  // Last 20 Match Batting Form-----
+  // Last 20 Match Bowling Form-----
+  // Carrier Overall Bowling Stats--
+  // Carrier Overall Batting Stats--
+  // Player Against Stadium Batting Stats----
+  // Player Against Stadium Bowling Stats----
+  // Player Against Team Batting Stats----
+  // Player Against Team Bowling Stats----
+
+  return <div>GetPrediction</div>;
+};
+
+export default GetPrediction;
